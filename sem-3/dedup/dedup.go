1184b65eb33541a234c82c174aa3a9007f07c5bf
Nikits Korovkin <ndkorovkin@edu.hse.ru>
2026-09-22 23:32:10 +0300

    sem-3: dedup

--- sem-3/dedup/dedup.go
+++ sem-3/dedup/dedup.go
@@ -4,7 +4,16 @@ package dedup
 
 // Deduplicate удаляет повторы, сохраняя порядок первых появлений.
 func Deduplicate(input []string) []string {
-	_ = input
-
-	return nil
+	if input == nil {
+		return nil
+	}
+	seen := make(map[string]struct{})
+	result := make([]string, 0, len(input))
+	for _, s := range input {
+		if _, ok := seen[s]; !ok {
+			seen[s] = struct{}{}
+			result = append(result, s)
+		}
+	}
+	return result
 }
